49851eff77202fb02d34d232f36fe873278729d6
IvanDandrea86 <ivan.d.andrea1@gmail.com>
2021-10-20 10:54:56 +0200

    Version 1.05 - all major bug fixe - added best score - still some probleme with first imgage of the hangman - still not enough consistance from mobile to desktop Version

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,8 +1,8 @@
 (() => {
     const alpha = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-    const vocab = ["console", "javascript", "react", "quiz", "nobody", "asynchronous", "languages"]
+    const vocab = ["console", "javascript", "react", "quiz", "nobody", "asynchronous", "languages","fetch","mammamia","superhero","limitless","panda"]
     const getRandomItem = arr => arr[Math.floor(Math.random() * arr.length)];
-
+    //Generate Random Word
     let generateRandomWord = (word) => {
         let secret = getRandomItem(word)
         console.log("Random Word Generated")
@@ -16,8 +16,8 @@
             head.appendChild(b)
         }
         return secret
-
     }
+    //Generate Keyboard
     let generateKeybord = ((alphabet) => {
         let head = document.getElementById("keybord")
         for (elem in alphabet) {
@@ -29,6 +29,7 @@
         }
         console.log("Keybord Generated")
     });
+    // Create main game section
     let createMain = () => {
         let intro = document.getElementById("enter_game")
         intro.style.display = "none";
@@ -37,7 +38,7 @@
         main.style.flexDirection = "column"
         console.log("Main Loaded")
     }
-
+    // Lose a life function
     let loseTray = (c) => {
         let t = document.getElementById("try_result")
         c++
@@ -59,23 +60,26 @@
         return c
     }
 
+    //Showing Letter FUnction
     let getLetter = ((i) => {
-
         let l = document.getElementById("secret-" + i)
         console.log(l)
         l.style.color = "#000"
         console.log("Lettra" + i)
 
     })
+
+    //Win Condition
     let isWin = ((w_counter, word_lenght) => {
         if (w_counter == word_lenght) return true
         else return false
     })
+    // Lost Condition
     let isLost = ((c) => {
         if (c === 6) return true
         else return false
     })
-
+    //Find element in string custom function
     let find = ((elem, string) => {
         var results = [];
         var idx = string.indexOf(elem);
@@ -87,11 +91,13 @@
         return results;
     })
 
+
+    //Hangman main game function
     let runHangman = () => {
-        var score
+        var score=0
         var count = 0
         var w = 0
-
+        
         console.log("It Works Hangman Started")
         createMain()
         const hang = generateRandomWord(vocab)
@@ -114,6 +120,7 @@
                         console.log("lost counter:", count)
                         if (isLost(count)) {
                             youLost()
+                            document.getElementById("best_result").innerHTML= checkBest(score, best_score)
                             showTheSecret(hang.length)
                         }
                     } else {
@@ -124,16 +131,21 @@
                             document.getElementById("let-" + char).disabled = true;
                             w++
                             console.log("win counter:", w)
-                            if (isWin(w, (hang.length))) { youWin() }
+                            if (isWin(w, (hang.length))) { 
+                                youWin() 
+                            }
                         }
                         let t = document.getElementById("try_result").innerHTML
-                        score = +(len * 12) - t
+                        score +=  ((len*5) -(t*0.5)) 
                         document.getElementById("score_result").innerHTML = score
+                        document.getElementById("best_result").innerHTML= checkBest(score, best_score)
                     }
 
                 })
         )
     }
+
+    // Win and Lose visualization Function
     let youWin = () => {
         document.querySelector("#game_end_title h2").textContent = "YOU WIN"
         console.log("You win")
@@ -141,18 +153,27 @@
     let youLost = () => {
         const keys = document.querySelectorAll('#keybord button')
         keys.forEach(elem => elem.disabled = true)
-
-
         document.querySelector("#game_end_title h2").textContent = "YOU LOST";
         console.log("You Lost");
     }
+    //Show secret word function
     let showTheSecret = ((index) => {
         for (i = 0; i < index; i++) {
             getLetter(i)
         }
 
     })
-
+    // Check Best Score Function
+    let checkBest=((s,new_s)=>{
+        if (new_s >s){
+            window.localStorage.setItem("counter-best-score",new_s)
+            return new_s
+            
+        }
+        window.localStorage.setItem("counter-best-score",s)
+        return s
+    })
+    // Reset  function
     let resetGame = () => {
         console.log("exit")
         document.getElementById("try_result").innerHTML = 0
@@ -172,6 +193,7 @@
         }
         parentNode.style.display = "none"
     }
+    // Play Again function
     let again = () => {
             console.log("again")
             document.getElementById("try_result").innerHTML = 0
@@ -188,22 +210,26 @@
                 key.removeChild(key.lastElementChild);
             }
             runHangman()
-
         }
         /*----------------
                Main
         ----------------*/
-
-    document.getElementById("start_button").addEventListener("click", () => {
-        runHangman()
+//Declare localStorage best score
+var best_score=window.localStorage.getItem("counter-best-score")
+document.getElementById("best_result").innerHTML=best_score
+//Enter the game button listener
+document.getElementById("start_button").addEventListener("click", () => {
+    runHangman()
     })
-    document.getElementById("back").addEventListener("click", () => {
-        resetGame()
-        console.log("restart")
+//Reset game button listener
+document.getElementById("back").addEventListener("click", () => {
+    resetGame()
+    console.log("restart")
     })
-    document.getElementById("again").addEventListener("click", () => {
-        again()
-        console.log("again")
+//Play Again button listener    
+document.getElementById("again").addEventListener("click", () => {
+    again()
+    console.log("again")
     })
 
 
